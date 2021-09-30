4ce0d5ffc93ecef1109ca97e86a9ef0364f76345
mayur249 <kundermayuresh249@gmail.com>
2021-09-30 13:18:57 +0530

    sent data to backend

--- client/src/components/Auth.jsx
+++ client/src/components/Auth.jsx
@@ -4,6 +4,8 @@ import axios from "axios";
 
 import signinImage from "../assets/signup.png";
 
+const cookies = new Cookies();
+
 const initialState = {
   fullName: "",
   username: "",
@@ -18,14 +20,39 @@ const Auth = () => {
   const [isSignup, setIsSignup] = useState(true);
 
   const handleChange = (e) => {
-    setForm({...form, [e.target.name]: e.target.value});
+    setForm({ ...form, [e.target.name]: e.target.value });
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log(form);
-  }
 
+    const { fullName, username, password, phoneNumber, avatarURL } = form;
+
+    const URL = "http://localhost:5000/auth";
+
+    const {
+      data: { token, userId, hashedPassword },
+    } = await axios.post(`${URL}/${isSignup ? "signup" : "signin"}`, {
+      fullName,
+      username,
+      phoneNumber,
+      password,
+      avatarURL,
+    });
+
+    cookies.set("token", token);
+    cookies.set("username", username);
+    cookies.set("fullName", fullName);
+    cookies.set("userId", userId);
+
+    if(isSignup) {
+      cookies.set("phoneNumber", phoneNumber);
+      cookies.set("avatarURL", avatarURL);
+      cookies.set("hashedPassword", hashedPassword);
+    }
+
+    window.location.reload();
+  };
 
   const switchMode = () => {
     setIsSignup((prevIsSignup) => !prevIsSignup);

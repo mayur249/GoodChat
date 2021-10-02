9ebca166654068cdb794e073dfb6c892d0446e1e
mayur249 <kundermayuresh249@gmail.com>
2021-10-02 13:01:35 +0530

    solved the name displaying issue

--- client/src/components/TeamChannelPreview.jsx
+++ client/src/components/TeamChannelPreview.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 import { Avatar, useChatContext } from "stream-chat-react";
 
-const TeamChannelPreview = ({ channel, type }) => {
+const TeamChannelPreview = ({
+  setActiveChannel,
+  channel,
+  type,
+  setToggleContainer,
+  setIsCreating,
+  setIsEditing,
+}) => {
   const { channel: activeChannel, client } = useChatContext();
 
   const ChannelPreview = () => (
@@ -14,15 +21,15 @@ const TeamChannelPreview = ({ channel, type }) => {
     const members = Object.values(channel.state.members).filter(
       ({ user }) => user.id !== client.userID
     );
-
+    console.log("Members[0] ",members[0]);
     return (
       <div className="channel-preview__item single">
         <Avatar
           image={members[0]?.user?.image}
-          name={members[0]?.user?.fullName}
+          name={members[0]?.user?.fullName || members[0]?.user?.name || members[0]?.user?.id}
           size={24}
         />
-        <p>{members[0]?.user?.fullName}</p>
+        <p>{members[0]?.user?.fullName || members[0]?.user?.name || members[0]?.user?.id}</p>
       </div>
     );
   };
@@ -35,10 +42,15 @@ const TeamChannelPreview = ({ channel, type }) => {
           : "channel-preview__wrapper"
       }
       onClick={() => {
-        console.log(channel);
+        setIsCreating(false);
+        setIsEditing(false);
+        setActiveChannel(channel);
+        if (setToggleContainer) {
+          setToggleContainer((prevState) => !prevState);
+        }
       }}
     >
-      {type === "team" ? <ChannelPreview/> : <DirectPreview/>}
+      {type === "team" ? <ChannelPreview /> : <DirectPreview />}
     </div>
   );
 };

c0a4dc45a64465eae46990cc997af64535e8cf10
mayur249 <kundermayuresh249@gmail.com>
2021-10-02 15:44:30 +0530

    Completed channel search component

--- client/src/components/ChannelSearch.jsx
+++ client/src/components/ChannelSearch.jsx
@@ -2,26 +2,61 @@ import React, { useState, useEffect } from "react";
 import { useChatContext } from "stream-chat-react";
 
 import { SearchIcon } from "../assets";
+import { ResultsDropDown } from "./";
 
-const ChannelSearch = () => {
+const ChannelSearch = ({ setToggleContainer }) => {
+  const { client, setActiveChannel } = useChatContext();
+  const [query, setQuery] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [teamChannels, setTeamChannels] = useState([]);
+  const [directChannels, setDirectChannels] = useState([]);
 
-    const [query, setQuery] = useState("");
-    const [loading, setLoading] = useState(false);
-
-    const getChannels = (text) => {
-        try {
-            // TODO: fetch channels
-        } catch (error) {
-            setQuery("")
-        }
+  useEffect(() => {
+    if (!query) {
+      setTeamChannels([]);
+      setDirectChannels([]);
     }
+  }, [query]);
 
-    const onSearch = (event) => {
-        event.preventDefault();
-        setLoading(true);
-        setQuery(event.target.value);
-        getChannels(event.target.value);
+  const getChannels = async (text) => {
+    try {
+      const channelResponse = client.queryChannels({
+        type: "team",
+        name: {
+          $autocomplete: text,
+        },
+        members: {
+          $in: [client.userID],
+        },
+      });
+      const userResponse = client.queryUsers({
+        id: { $ne: client.userID },
+        name: { $autocomplete: text },
+      });
+
+      const [channels, { users }] = await Promise.all([
+        channelResponse,
+        userResponse,
+      ]);
+
+      if (channels.length) setTeamChannels(channels);
+      if (users.length) setDirectChannels(users);
+    } catch (error) {
+      setQuery("");
     }
+  };
+
+  const onSearch = (event) => {
+    event.preventDefault();
+    setLoading(true);
+    setQuery(event.target.value);
+    getChannels(event.target.value);
+  };
+
+  const setChannel = (channel) => {
+    setQuery("");
+    setActiveChannel(channel);
+  };
 
   return (
     <div className="channel-search__container">
@@ -33,10 +68,20 @@ const ChannelSearch = () => {
           className="channel-search__input__text"
           placeholder="Search"
           type="text"
-          value = {query}
-          onChange = {onSearch}
+          value={query}
+          onChange={onSearch}
         />
       </div>
+      {query && (
+        <ResultsDropDown
+          teamChannels={teamChannels}
+          directChannels={directChannels}
+          loading={loading}
+          setChannel={setChannel}
+          setQuery={setQuery}
+          setToggleContainer={setToggleContainer}
+        />
+      )}
     </div>
   );
 };
